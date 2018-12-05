bc7e4d6e6ce29046232b096a8e41e6ff47b164c6
root <root@localhost.localdomain>
2018-12-05 20:06:18 +0100

    Creado en azure primera vez

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "azurerm_network_security_group" "test" {
 resource "azurerm_virtual_network" "test" {
   name                = "virtualNetwork01"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.190.0.0/16"]
   location            = "West US"
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  dns_servers         = ["10.100.80.111", "10.100.80.112"]
   
   }
 
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "test1" {
   name           = "subnet1"
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = "10.190.1.0/24"
  
 }
 resource "azurerm_subnet" "test2" {
